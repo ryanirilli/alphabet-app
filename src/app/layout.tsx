244a4134c4763db83fc84ad55560264a48dea55f
Ryan Irilli <ryanirilli@gmail.com>
2024-06-25 11:25:44 -0700

    add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import GoogleAdsense from "@/components/google-adsense";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <GoogleTagManager gtmId="G-6BGBM7EXKC" />
       <body className={`${inter.className} sm:overflow-auto`}>
         <ThemeProvider
           attribute="class"
