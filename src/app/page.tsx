e475bd2496fe9b880ecafc8c614993b2b4f95c3d
Ryan Irilli <ryanirilli@gmail.com>
2024-06-30 14:44:06 -0700

    fix landing page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <main className="bg-gradient-to-br from-blue-500 to-cyan-500 min-h-screen">
       <section className="xl:hidden bg-homepage-hero bg-contain bg-no-repeat bg-right-top min-h-64" />
-      <section className="xl:bg-homepage-hero bg-contain bg-no-repeat bg-right-top flex flex-col justify-center items-center py-16 px-4 xl:py-32">
+      <section className="xl:bg-homepage-hero bg-[length:680px_393px] 2xl:bg-contain bg-no-repeat bg-right-top flex flex-col justify-center items-center py-16 px-4 xl:py-32">
         <div className="container">
           <HomePageHeadline />
         </div>
