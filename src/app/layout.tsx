cac4cd66d06b4e9a5a9e0c1d3af5180199c6ea78
Ryan Irilli <ryanirilli@gmail.com>
2024-09-10 14:17:30 -0700

    add social share metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,23 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Learn the alphabet and have fun doing it",
   description:
-    "Your kids are always taking your phone, they might as well learn something while they're at it.",
+    "Your kids are always taking your phone, they might as well learn something while they're at it!",
+  openGraph: {
+    title: "Learn the alphabet and have fun doing it",
+    description: `Your kids are always taking your phone, they might as well learn something while they're at it!`,
+    images: [
+      {
+        url: "https://www.abcflashcards.xyz/social-share.png",
+        width: 1200,
+        height: 630,
+        alt: "Image description",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: ["https://www.abcflashcards.xyz/social-share.png"],
+  },
 };
 
 export default function RootLayout({
